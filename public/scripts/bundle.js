371d290fc056425cb883469af3522456d1838a72
unknown <sazwerling@gmail.com>
2022-02-03 10:28:36 -0600

    set up boilerplate

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -6,6 +6,7 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 
+//typo for .gitignore fixed
 var name = "Slim";
 console.log(name);
 
